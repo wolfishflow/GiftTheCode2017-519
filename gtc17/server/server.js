dfb0f19e4c44ac0867b71d5590b88232d6cd26ca
dburandt <behringer.19@gmail.com>
2017-11-03 23:53:09 -0400

    cleanup

--- gtc17/server/server.js
+++ gtc17/server/server.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var morgan = require('morgan')
 var bodyParser = require('body-parser');
-
 var memberRouter = require('./routes/member');
 
 var app = express();
 var port = 8080;
+
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-
 app.use('/api', memberRouter);
 
 app.listen(port, () => {
