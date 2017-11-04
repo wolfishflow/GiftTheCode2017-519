ac5f19500d5a3ba28a4f9c2a648facaae8f81198
dburandt <behringer.19@gmail.com>
2017-11-04 15:02:06 -0400

    more signup wizard ui changes

--- gtc17/src/app/shared/Member.ts
+++ gtc17/src/app/shared/Member.ts
@@ -1,4 +1,17 @@
+
 export class Member {
-  id: number;
-  name: string;
+  constructor(
+  public memberId: number,
+  public firstName: string,
+  public lastName: string,
+  public birthdate: string,
+  public streetAddress: string,
+  public city: string,
+  public province: string,
+  public country: string,
+  public postalcode: string,
+  public email: string,
+  public preferredPhone: string,
+  public membershipDetails: string
+  ) {}
 }
--- gtc17/src/app/signup/signup.component.ts
+++ gtc17/src/app/signup/signup.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SignupService } from './signup.service';
 import { FormBuilder, FormGroup, Validators, AbstractControl, FormControl } from '@angular/forms';
+import { Member } from '../shared/Member';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/takeUntil';
 
 @Component({
   selector: 'app-signup',
@@ -11,6 +14,11 @@ export class SignupComponent implements OnInit {
 
   public currentStep: number = 0;
   public memberForm: FormGroup;
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
+
+  public individualSelected: boolean = true;
+  public mailAddressSelected: boolean = true;
+  public homeAddress: boolean = true;
 
   constructor(private signupService: SignupService,
               private formBuilder: FormBuilder) { }
@@ -20,16 +28,60 @@ export class SignupComponent implements OnInit {
       firstName: ['', [Validators.required, Validators.minLength(1)]],
       lastName:  ['', [Validators.required, Validators.minLength(1)]],
       birthDate: ['', [Validators.required]],
-      street: ['', [Validators.required, Validators.minLength(1)]],
+      streetAddress: ['', [Validators.required, Validators.minLength(1)]],
       city: ['', [Validators.required, Validators.minLength(1)]],
-      postalCode: ['', [Validators.required, Validators.minLength(1)]],
+      province: ['', [Validators.required, Validators.minLength(1)]],
+      country: ['', [Validators.required, Validators.minLength(1)]],
+      postalcode: ['', [Validators.required, Validators.minLength(1)]],
+      email: ['', [Validators.required, Validators.minLength(1)]],
+      preferredPhone: ['', [Validators.required, Validators.minLength(1)]],
+      membershipDetails: ['', [Validators.required, Validators.minLength(1)]]
     });
   }
 
   submitMemberForm() {
-    let firstName = this.memberForm.controls["firstName"].value;
     
-    console.log(firstName);
+    let memberId;
+    let firstName = this.memberForm.controls["firstName"].value;
+    let lastName = this.memberForm.controls["lastName"].value;
+    let birthdate;
+    // TODO: birthdate
+    let streetAddress = this.memberForm.controls["streetAddress"].value;
+    let city = this.memberForm.controls["city"].value;
+    let postalcode = this.memberForm.controls["postalcode"].value;
+    let email = this.memberForm.controls["email"].value;    
+    let province = this.memberForm.controls["province"].value;
+    let country = this.memberForm.controls["country"].value;
+    let preferredPhone = this.memberForm.controls["preferredPhone"].value;
+    let membershipDetails = this.memberForm.controls["membershipDetails"].value;
+
+    let member = new Member(memberId, firstName, lastName, birthdate, streetAddress, city, province, country, 
+                            postalcode, email, preferredPhone, membershipDetails);
+    console.log(member);
+    //this.createMember(member)
+  }
+
+  createMember(member: Member) {
+    this.signupService.createMember(member).takeUntil(this.ngUnsubscribe).subscribe(
+      member => { }
+    )
+  }
+
+  toggleIndividualSelection() {
+    this.individualSelected = !this.individualSelected;
+  }
+
+  toggleMailSelection() {
+    this.mailAddressSelected = !this.mailAddressSelected;
+  }
+
+  toggleAddressSelection() {
+    this.homeAddress = !this.homeAddress;
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 
   //ngif memForm.controls.fieldname.hasError{}
