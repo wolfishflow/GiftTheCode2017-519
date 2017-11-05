7755ddd47b0296ef9c48ab1d4b95aaf866cf6641
dburandt <behringer.19@gmail.com>
2017-11-05 11:08:31 -0500

    added admin page

--- gtc17/src/app/admin/admin.component.ts
+++ gtc17/src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
+import { AdminService } from './admin.service';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'admin',
@@ -7,13 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  public showCharts: boolean = true;
+  constructor(private adminService: AdminService,
+              private http: HttpClient) { }
 
   ngOnInit() {
   }
 
   submitSearch() {
-    
+    this.showCharts = false;
+  }
+
+  exportCsv() {
+    console.log("got ehre");
+    //this.adminService.createCSV();
+    this.http.get("http://localhost:8080/api/export").subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
---
+++ gtc17/src/app/admin/admin.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Http } from '@angular/http';
+import { Observable } from "rxjs/Observable";
+import { Member } from "../shared/Member";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class AdminService {
+
+  public apiRoute = "http://localhost:8080/api/export";
+
+  constructor(private http: Http) {}
+
+  createCSV() {
+    console.log("and i got here");
+    this.http.get(this.apiRoute);
+  }
+
+  private formatErrors(error: any) {
+    return Observable.throw(error.json());
+  }
+
+}
